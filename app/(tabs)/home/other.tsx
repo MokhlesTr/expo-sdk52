70303306bda7d5207c884fe83b2977d88608fc17
MokhlesTr <mokhlestarmiz001@gmail.com>
2025-01-02 14:13:42 +0100

    code cleaning

--- app/(tabs)/home/other.tsx
+++ app/(tabs)/home/other.tsx
@@ -2,15 +2,14 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
 
-const settings = () => {
+const other = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>settings</Text>
-      <Link href="/home/other">Push settings</Link>
     </View>
   );
 };
 
-export default settings;
+export default other;
 
 const styles = StyleSheet.create({});
