4cf46e9d660cad3d251f651b973f58966c8585fe
MokhlesTr <mokhlestarmiz001@gmail.com>
2025-01-02 14:51:00 +0100

    header customize

--- app/(tabs)/home/_layout.tsx
+++ app/(tabs)/home/_layout.tsx
@@ -1,8 +1,22 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { Text, TouchableOpacity } from "react-native";
 
 const Stacklayout = () => {
+  const router = useRouter();
   return (
-    <Stack>
+    <Stack
+    //   screenOptions={{
+    //     headerTitle: "My Custom Header Title",
+    //     headerStyle: { backgroundColor: "#f0f0f0" },
+    //     headerTintColor: "#000000",
+    //     headerTitleStyle: { fontWeight: "regular" },
+    //     headerLeft: () => (
+    //       <TouchableOpacity onPress={() => router.back()}>
+    //         <Text>Back</Text>
+    //       </TouchableOpacity>
+    //     ),
+    //   }}
+    >
       <Stack.Screen name="index" options={{ headerTitle: "Home Screen" }} />
     </Stack>
   );
--- app/(tabs)/home/other.tsx
+++ app/(tabs)/home/other.tsx
@@ -1,11 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 
 const other = () => {
+  const router = useRouter();
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>settings</Text>
+      <Pressable onPress={() => router.back()}>
+        <Text>Go Back</Text>
+      </Pressable>
     </View>
   );
 };
