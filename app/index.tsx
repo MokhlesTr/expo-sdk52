f61fd28e199b13a2345ef8baec6e362d95c4dfd7
MokhlesTr <mokhlestarmiz001@gmail.com>
2025-01-02 14:22:51 +0100

    startPage

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
-import { Redirect } from "expo-router";
+import { Link, Redirect } from "expo-router";
+import { Pressable, Text, TextInput, View } from "react-native";
 
 const StartPage = () => {
-  return <Redirect href="/(tabs)/home" />;
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>LOGIN</Text>
+      {/* <Redirect href="/(tabs)/home" /> */}
+      <Link href="/(tabs)/home">
+        <Text>Go to Home</Text>
+      </Link>
+    </View>
+  );
 };
 export default StartPage;
