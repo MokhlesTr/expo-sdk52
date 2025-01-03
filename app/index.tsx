ea26632bf59329bff75d78fa72b79f2a1ae46ee6
MokhlesTr <mokhlestarmiz001@gmail.com>
2025-01-03 10:35:38 +0100

    replace & asChild concepts

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { Link, Redirect } from "expo-router";
-import { Pressable, Text, TextInput, View } from "react-native";
+import {
+  Pressable,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 const StartPage = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       {/* <Text>LOGINssss</Text> */}
       {/* <Redirect href="/(tabs)/home" /> */}
-      <Link href="/(tabs)/home">
-        <Text>Go to Home</Text>
+
+      <Link href="/(tabs)/home" replace asChild>
+        {/* replace ==> route without back */}
+        {/* asChild ==> custom component as a clickable */}
+        <TouchableOpacity>
+          <Text>Go to Home</Text>
+        </TouchableOpacity>
       </Link>
     </View>
   );
