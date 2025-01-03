2b7a973b6fc6c274d846a66b31b65acf0d2f0652
MokhlesTr <mokhlestarmiz001@gmail.com>
2025-01-03 12:14:41 +0100

    minor fix

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,6 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import { Link, useGlobalSearchParams } from "expo-router";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
 
 const Home = () => {
   const { bg } = useGlobalSearchParams<{ bg?: string }>();
@@ -19,10 +23,15 @@ const Home = () => {
       }}
     >
       <Link href="/home/settings">Push settings</Link>
+      <TouchableOpacity onPress={() => setSelectedBg("")}>
+        {selectedBg.length > 0 && <Text style={styles.styledText}>Reset</Text>}
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  styledText: { color: "white", fontWeight: "600", fontSize: hp("2.5%") },
+});
