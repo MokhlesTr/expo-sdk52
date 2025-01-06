81d6c9b7298b1cd8bd96768aa038adddde366cd5
MokhlesTr <mokhlestarmiz001@gmail.com>
2025-01-06 10:26:15 +0100

    Redirect auth condition

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,10 +4,10 @@ import {
   ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
